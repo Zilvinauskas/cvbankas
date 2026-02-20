bf35bf553ab21f8c336ba8bcda230c85d3dcc193
Valdovas <z.kamarauskas@gmail.com>
2026-02-20 09:20:17 +0000

    added parse discovery, .gitignore

--- scrape_discovery.py
+++ scrape_discovery.py
@@ -8,7 +8,6 @@ def main():
         
         page = browser.new_page()
         page_number = 1
-        jobs = []
         title = 0
   
         while True:
@@ -18,41 +17,20 @@ def main():
             page.goto(requested_url)
             
             # delay
-            page.wait_for_load_state("networkidle")
-            
-            # give html to Parsel
-            html = page.content()
-            selector = Selector(text=html)
-            
-            # Extract data using CSS 
-            job_cards = selector.css("a.list_a")
-            
-            # Extract stuff from job listing card
-            for card in job_cards:
-                item = {
-                    "link": card.css("::attr(href)").get()
-                }               
-                # go to job ad
-                page.goto(item["link"])
-                page.wait_for_load_state("networkidle")
-                print(f"I am currently at: {page.url}")
-                
-                # create and write file                
-                with open(f"html/file{title}.html", "w", encoding="utf-8") as file:
-                    file.write(page.content())
-                
-                #update title name
-                title += 1               
+            page.wait_for_load_state("networkidle")        
+
                 
             # last page check
             if page.url != requested_url:
                 print("\n done \n ------------------------\n")
                 break
-            # for testing purposes - only scan ads on first page of cvbankas
-            if page_number >= 1:
-                break
+            
+            #save htmls to html dir
+            with open(f"htmls/file{title}.html", "w", encoding="utf-8") as file:
+                file.write(page.content())
 
             page_number += 1
+            title += 1
                  
         
 if __name__ == "__main__":
